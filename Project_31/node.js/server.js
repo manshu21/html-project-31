f0ec206e1bdde3418b4020660a5bb4c7c1159b20
Manshu <manshukumar505@gmail.com>
2026-08-27 20:43:33 +0530

    Update server.js

--- Project_31/node.js/server.js
+++ Project_31/node.js/server.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 function serveFile(fileName, res, statusCode = 200) {
   fs.readFile(fileName, (err, data) => {
     if (err) {
-      console.error(`Error reading ${fileName}:`, err);
+      console.log("Error:", err);
 
       res.writeHead(500, {
-        "Content-Type": "text/html",
+        "Content-Type": "text/html"
       });
 
       res.end("<h1>500 - Internal Server Error</h1>");
@@ -15,7 +15,7 @@ function serveFile(fileName, res, statusCode = 200) {
     }
 
     res.writeHead(statusCode, {
-      "Content-Type": "text/html",
+      "Content-Type": "text/html"
     });
 
     res.end(data);
@@ -23,17 +23,22 @@ function serveFile(fileName, res, statusCode = 200) {
 }
 
 const server = http.createServer((req, res) => {
+
   if (req.url === "/") {
     serveFile("home.html", res);
+
   } else if (req.url === "/about") {
     serveFile("about.html", res);
+
   } else if (req.url === "/contact") {
     serveFile("contact.html", res);
+
   } else {
     serveFile("404.html", res, 404);
   }
+
 });
 
 server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log("Server is running on http://localhost:3000");
 });
